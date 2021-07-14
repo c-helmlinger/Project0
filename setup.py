3381cefdd294e5a8bef356ce41ebdf8ef7f5d267
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2021-07-14 15:11:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
   name='Project0',
   author='Project 0',
   version='1.0.0',
-  description='Takes outsourced data to predict housing prices, then visualizes."
+  description='Uses outsourced data to predict housing prices, then visualizes."
   packages='Project0'
 )
