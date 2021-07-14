656a201577b360f629237147c63d68370786c6e4
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2021-07-14 11:04:10 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+  name='LinearRegression_combined_v1_highest',
+  author='Jessica Zhang',
+  version='1.0.0',
+  description='Uses outsourced data to predict housing prices, then visualizes."
+  packages='LinearRegression_combined_v1_highest.ipynb'
