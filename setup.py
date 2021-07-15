ca82840586331e2f8fdace2b3e84c610a7e0b21a
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2021-07-14 21:20:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
   name='Neural_Network',
   author='Kevin Zheng',
   version='1.0.0',
-  description='Uses outsourced data to predict housing prices, then visualizes."
-  packages='Neural_Network'
+  description='Uses outsourced data to predict housing prices, then visualizes.",
+  packages=['Neural_Network']
+  package_dir={'Neural_Network':'Scripts'}
 )
