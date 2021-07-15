60a7221d901c0d6a2fb3aa09fda0306c086869a4
c-helmlinger <86612309+c-helmlinger@users.noreply.github.com>
2021-07-15 05:38:37 -0700

    Create setup.py

---
+++ Scripts/setup.py
@@ -0,0 +1,10 @@
+pip install setuptools
+from setuptools import setup
+
+setup(
+  name='Linear_Regression',
+  author='Jessica Zhang',
+  version='1.0.0',
+  description='Uses outsourced data to predict housing prices, then visualizes.',
+  packages='Linear_Regression',
+)
